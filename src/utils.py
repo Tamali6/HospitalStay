8ef7f24bbd353b5cd44bf325c3a8da5675c6dfdc
Tamali6 <tamali@cse.iitb.ac.in>
2025-03-09 21:31:45 +0100

    Corrected mistakes

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import yaml
 
+# Loads configuration settings from a YAML file
 def load_config(config_path="config.yaml"):
-    """Loads configuration settings from a YAML file."""
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
     return config
